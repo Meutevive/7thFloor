a7d39e8de34518a4efd8e863ba98c0d9b2e74424
delm0128 <e2palmes@gmail.com>
2023-03-25 18:19:13 +0100

    add created_at on user

--- src/main/java/com/cinamatheque/cinamatheque/listeners/UserEventListener.java
+++ src/main/java/com/cinamatheque/cinamatheque/listeners/UserEventListener.java
@@ -1,7 +1,6 @@
 package com.cinamatheque.cinamatheque.listeners;
 
-
-import com.cinamatheque.cinamatheque.model.Comment;
+import com.cinamatheque.cinamatheque.model.User;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
 import org.springframework.stereotype.Component;
@@ -9,10 +8,9 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Component
-public class CommentListener extends AbstractMongoEventListener<Comment> {
-
+public class UserEventListener extends AbstractMongoEventListener<User> {
     @Override
-    public void onBeforeConvert(BeforeConvertEvent<Comment> event){
+    public void onBeforeConvert(BeforeConvertEvent<User> event){
         super.onBeforeConvert(event);
 
         Date datenow = new Date();
--- src/main/java/com/cinamatheque/cinamatheque/model/User.java
+++ src/main/java/com/cinamatheque/cinamatheque/model/User.java
@@ -10,8 +10,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.Assert;
-
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 
@@ -30,6 +30,7 @@ public class User implements UserDetails {
     private String address;
     private String country;
     private String phone;
+    private Date created_at;
 
     /*
      * each @param must not be {@literal null} or empty.
